40e86efc78178615610a0c0c9c9aeb04bfb64632
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-03 20:11:21 +0000

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1> HELLO, world </h1>
+      <NavBar />
     </div>
   );
 }
