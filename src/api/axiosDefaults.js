b8d27f287b58f46372a98bc88609bd40f3c1553e
Mohamed Ali Abdikarim <moi-ali@hotmail.com>
2024-07-06 10:51:18 +0000

    added sifn in page

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+axios.defaults.baseURL = 'https://contentsharing-api-7b3cd872bc62.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
